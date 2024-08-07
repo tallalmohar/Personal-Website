48615d36c9bf44e52d5d72a99865358e1967b9b2
tallalmohar <tallalmoohar@gmail.com>
2024-08-07 15:20:20 -0700

    new changes

---
+++ script.js
@@ -0,0 +1,8 @@
+
+var typed = new Typed('.element', {
+    strings: ["I am a 3rd year SoSy Student @ SFU.", "Currently interested in Game Dev.", "Active in Basketball, Hiking and the Gym.",],
+    typeSpeed: 100,
+    backSpeed: 50,
+    loop: true
+  });
+
